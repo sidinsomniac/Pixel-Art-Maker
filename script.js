9bef312eaa7b68f23b1fcb83c9bfae37fb48e00c
sidinsomniac <chatterjeesid1993@gmail.com>
2018-02-25 23:06:49 +0530

    Enabled color drawing and made several functions

--- script.js
+++ script.js
@@ -23,12 +23,36 @@
 
 // $('input[type="submit"]').on('click',createGrid);
 
+	
+
 function createGrid(event){
 	reset();
-	var canvas = document.querySelector('#pixelCanvas');
-	var rows = document.querySelector('#input_height').value;
-	var columns = document.querySelector('#input_width').value;
+	updateGrid();
+	draw();	
+	event.preventDefault();
+}
 
+
+
+function draw(){
+	var newColor = document.querySelector('#colorPicker');
+	var cells = document.querySelectorAll('td');
+	for(var x = 0; x < cells.length ; x++){
+		cells[x].setAttribute("draggable",true);
+		cells[x].addEventListener('dragover', function(event) {
+			this.style.backgroundColor=newColor.value;
+			event.preventDefault();
+			event.dataTransfer.effectAllowed= 'copyMove';
+            event.dataTransfer.dropEffect= 'move';
+			return false;
+		});
+	}
+}
+
+function updateGrid(){	
+	var canvas = document.querySelector('#pixelCanvas');
+	var columns = document.querySelector('#input_width').value;
+	var rows = document.querySelector('#input_height').value;
 	for(var i = 0; i < rows ; i++){
 	var row = document.createElement("tr");
 		canvas.appendChild(row);		
@@ -37,18 +61,6 @@ function createGrid(event){
 			row.appendChild(cell);
 		}
 	}
-	var cells = document.querySelectorAll('td');
-	for(var x = 0; x < cells.length ; x++){
-		cells[x].setAttribute("draggable",true);
-		cells[x].addEventListener('dragover', function(event) {
-			this.style.background = "black";
-			event.preventDefault();
-			event.dataTransfer.effectAllowed= 'copyMove';
-            event.dataTransfer.dropEffect= 'move';
-			return false;
-		});
-	}
-	event.preventDefault();	
 }
 
 function reset(){
@@ -62,6 +74,6 @@ function reset(){
 		}
 
 	}
-	
+
 
 document.querySelector('input[type="submit"]').addEventListener('click',createGrid);
